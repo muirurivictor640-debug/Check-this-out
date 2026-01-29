8376bfae10e0a1e0c9b3338d3f5823742cad80fb
muirurivictor640-debug <muirurivictor640@gmail.com>
2026-01-29 18:52:28 +0300

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://youtu.be/8A7-54UguKg?si=G5FJpnNQazg_X14v", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dbarjrlaj/video/upload/v1769701858/James_Arthur_-_Say_You_Wont_Let_Go_CeeNaija.com__brgwnv.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
